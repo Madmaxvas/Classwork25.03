ddca5388a244329b3e3728610438b1b6501acd38
Madmaxvas <63355084+Madmaxvas@users.noreply.github.com>
2026-03-25 14:52:39 +0300

    maint test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 include pe-vector.hpp
+#include <iostream>
+  
+bool test 1()
+{
+  knk::Vector< int > v;
+  return true;
+}
 
 int main()
 {
-  knk::Vector< int > v;
+  using test_t = bool(*)();
+  test_t tests[] = {
+    test1
+  };
+  constexpr size_t count = sizeof(tests) / sizeof(test_t);
+  for (size_i = 0; i < count; ++i){
+    bool r = tests[i]();
+    if (r!) {
+      std::cout << "Failed: " << i << "\n";
+    }
+  }
 }
