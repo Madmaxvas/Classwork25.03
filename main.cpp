027737e23daef3b669160df24f8d6bb3e7a3270a
Madmaxvas <63355084+Madmaxvas@users.noreply.github.com>
2026-04-08 12:14:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,51 +1,57 @@
+#include "pe-vector.hpp"
 #include <iostream>
-#include pe-vector.hpp
-using knk::Vector;
 
-bool testConstructAndDestruct(const char ** pname)
+bool testConstAndDest(const char ** pname)
 {
   *pname = __func__;
   knk::Vector< int > v;
   return true;
 }
-bool testDefaultVectorIsEmpty(const char ** pname)
+
+bool testDefVecIsEmpty(onst char ** pname)
 {
   *pname = __func__;
   Vector< int > v;
   return v.isEmpty();
 }
-bool testSizeOfEmptyVector(const char ** pname)
-{
-  *pname = __func__;
-  Vector< int > v;
-  return !v.getSize();
+
+bool testSizeOfEmptyVec(const char** pname){
+ *pname = __func__;
+ Vector< int > v;
+ return !v.getSize();
 }
-bool testSizeOfNonEmptyVector(const char ** pname)
-{
-  return false; 
+
+bool testSizeOfNonEmptyVec(const char** pname){
+ *pname = __func__;
+ constexpr size_t size = 2;
+ Vector< int > v(2ull, 5);
+ return v.getSize() == 2ull;
 }
+
 
 int main()
 {
   using test_t = bool(*)(const char **);
-  using case_t = std::pair< test_t , const char *>;
-  test_t tests[] = {
-    { testConstructAndDestruct, "Vector must be default constructable" },
-    { testDefaultVectorIsEmpty, "Default constructed vector must be empty" },
-    { testSizeOfEmptyVector, "Size of empty vector must be zero" },
-    { testSizeOfNonEmptyVector, "Size of non-empty vector must be greater than zero" },
+  using case_t = std::pair< test_t, const char *>;
+  case_t tests[]{
+    { testConstAndDest },
+    { testDefVecIsEmpty },
+    { testSizeOfEmptyVec },
+    { testSizeOfNonEmptyVec }
+    
   };
-  constexpr size_t count = sizeof(tests) / sizeof(test_t);
+  constexpr size_t count = sizeof(tests) / sizeof(case_t);
   size_t failed = 0;
-  for (size_i = 0; i < count; ++i){
-    const char * testName = nullptr;
-    bool r = tests[i].first(&testName);
-    if (r!) {
-      ++failed;
+  for (size_t i = 0; i < count; ++i){
+    bool r = tests[i]()
+    const chat ** testName = nullptr;
+    bool r = tests[i](&testName);
+    if (!r) {
+      ++failed
       std::cout << "Failed: " << testName << "\n";
-      std::cout << "\t" << test[i].second << "\n";
+      std::cout << "\t" << tests[i].second << "\n";
     }
   }
-  std::cout << "Summary: " << (count - failed) << " passed\n";
-  std::cout << "\t" << count << " total\n";
+  std::cout << "Summary:\n\t " << (count - failed) << "passed\n";
+  std::cout << "\t" << count << "total\n"
 }
