b510e8b7af4f4c71799c41b3143cd4d4373c472c
Madmaxvas <63355084+Madmaxvas@users.noreply.github.com>
2026-04-08 12:14:49 +0300

    Fix syntax errors and improve Vector class implementation

--- pe-vector.hpp
+++ pe-vector.hpp
@@ -1,34 +1,64 @@
 #ifndef PE_VECTOR_HPP
 #define PE_VECTOR_HPP
-#include <cstddef>
-namespace knk
-{
+#include <cstdef>
+
+namespace knk {
   template< class T >
   class Vector {
-   public:
+    public:
     ~Vector();
     Vector();
+    Vector(const Vector< T >& rhs) = delete;
+    Vector(size_t size, const T& value);
 
-    bool isEmpty() const noexept;
-    size_t getSize() const noexept;
+    Vector< T >& operator=(const Vector< T >& rhs) = delete;
 
-   private:
+    bool isEmpty() const noexcept;
+    size_t getSize() const noexcept;
+
+    //Реализовать и протестировать
+    size_t getCapacity() const noexcept;
+    void pushBack(const T&);
+    void popBack();
+
+    private:
     T* data;
-    size_t size, capacity;
+    size_t size_, capacity_;
+
+    explicit Vector(size_t size);
   };
 }
+
 template< class T >
-bool knk::Vector< T >::isEmpty() const noexept{
+knk::Vector< T >::Vector(size_t size):
+  data_(size ? new T[size] : nullptr),
+  size_(size),
+  capacity_(size)
+{}
+
+template< class T >
+knk::Vector< T >::Vector(size_t size, const T& value):
+  Vector(size)
+{
+  for (size_t i = 0; i < size; ++i){
+    data_[i] = value;
+  }
+}
+
+template< class T >
+bool knk::Vector< T >::getSize() const noexcept {
+  return size_;
+}
+
+template< class T >
+bool knk::Vector< T >::isEmpty() const noexcept {
   return !size_;
-} 
-template< class T >
-knk::Vector< T >::Vector(): 
-  data_(nullptr),
+}
+knk::Vector< T >::Vector():
+  data(nullptr),
   size_(0),
   capacity_(0)
-{}
-template< class T >
-knk::Vector< T >::~Vector() {
-  delete [] data_;
+  delete[] data_;
 }
+
 #endif
